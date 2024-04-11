2d7f76e0b89143e258ee91b6eb2781a0c36f7fa6
adarsheventhq <adarsh.mamgain@eventhq.com>
2024-04-12 03:48:01 +0530

    added assets and metadata tags

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,30 @@
+import { Metadata } from "next";
 import Link from "next/link";
+
+export const metadata: Metadata = {
+  title: "WebClones",
+  description: "WebClones of WebSite :) Made by @adarsh-mamgain",
+  authors: [{ name: "Adarsh Mamgain", url: "https://x.com/adarsh_mamgain" }],
+  openGraph: {
+    title: "WebClones",
+    description: "WebClones of WebSite :) Made by @adarsh-mamgain",
+    siteName: "Webtools",
+    images: "https://webclones.vercel.app/public/icon.svg",
+  },
+  twitter: {
+    site: "https://x.com/adarsh_mamgain",
+    creator: "Adarsh Mamgain",
+    creatorId: "adarsh_mamgain",
+    description: "WebClones of WebSite :) Made by @adarsh-mamgain",
+    title: "WebClones",
+    images: "https://webclones.vercel.app/public/icon.svg",
+  },
+  robots: "index, follow",
+};
 
 export default function Home() {
   return (
-    <main className="p-2">
+    <main className="min-h-screen p-4">
       <h1 className="text-3xl mb-4">Website Clones ⬇️</h1>
       <div className="flex flex-col text-xl font-bold">
         <Link href="netflix">
@@ -10,6 +32,12 @@ export default function Home() {
             Netflix
           </div>
         </Link>
+      </div>
+
+      <div className="absolute bottom-10 text-green-300">
+        <p>
+          Made by <a href="https://x.com/adarsh_mamgain">@adarsh_mamgain</a>
+        </p>
       </div>
     </main>
   );
